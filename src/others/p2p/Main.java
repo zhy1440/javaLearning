9b3898c6b16ba7886da015b947f4caa15e4316a8
cathy <Cathy@Cathy-PC>
2019-02-22 01:57:27 +0800

    add p2p src

---
+++ src/others/p2p/Main.java
@@ -0,0 +1,46 @@
+package others.p2p;
+
+public class Main {
+    private static int TEM_THOUSAND = 10000;
+
+    public static class Calculator {
+        private int capital;
+        private double rate;
+        private double salaryRate;
+        private double income;
+        private String capitalText;
+
+        Calculator(int capital, double rate, double salaryRate) {
+            this.capital = capital;
+            this.rate = rate;
+            this.salaryRate = salaryRate;
+        }
+
+        double getIncome() {
+            this.income = this.capital * this.rate + this.capital * this.salaryRate * 2;
+            return income;
+        }
+
+        String getCapitalText() {
+            this.capitalText = capital / TEM_THOUSAND + "w";
+            return capitalText;
+        }
+    }
+
+    public static void main(String[] args) {
+        double highRate = 0.085;
+        double lowRate = 0.07;
+        double oldSalary = 1600 / 100000.0;
+        double highSalary = 4000.0 / 100000;
+        double lowSalary = 260.0 / 10000;
+
+        Calculator c1 = new Calculator(10 * TEM_THOUSAND, highRate, oldSalary);
+        Calculator c2 = new Calculator(2 * TEM_THOUSAND, lowRate, lowSalary);
+        Calculator c3 = new Calculator(10 * TEM_THOUSAND, highRate, highSalary);
+
+
+        System.out.println(c1.getCapitalText() + ": " + c1.getIncome());
+        System.out.println(c2.getCapitalText() + ": " + c2.getIncome());
+        System.out.println(c3.getCapitalText() + ": " + c3.getIncome());
+    }
+}
